c9dca90e012b1662f7f8319f6c851a0a5278b75e
animevn <haanhth21@gmail.com>
2019-12-21 18:22:46 +0700

    implicit intent to open phone

--- app/src/main/java/com/haanhgs/app/intentkeyboardsenddemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/intentkeyboardsenddemo/MainActivity.java
@@ -1,14 +1,53 @@
 package com.haanhgs.app.intentkeyboardsenddemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethod;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etPhone)
+    EditText etPhone;
+
+    private void startIntent(Intent intent){
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+    private void dialPhone(){
+        if (!TextUtils.isEmpty(etPhone.getText())){
+            String phoneNumber = "tel:" + etPhone.getText().toString();
+            Uri uri = Uri.parse(phoneNumber);
+            Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+            startIntent(intent);
+        }
+    }
+
+    //edittext should add ime option to send
+    private void handleEditText(){
+        etPhone.setOnEditorActionListener((v, actionId, event) -> {
+            boolean result =  false;
+            if (actionId == EditorInfo.IME_ACTION_SEND){
+                dialPhone();
+                result = true;
+            }
+            return result;
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        handleEditText();
     }
 }
